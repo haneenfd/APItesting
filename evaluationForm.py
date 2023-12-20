a067cee9326ccf5b5e45989d3a21693fcae75a58
haneenth <hanendiab11@gmail.com>
2023-12-20 12:52:44 +0200

    evaluation form test

---
+++ evaluationForm.py
@@ -0,0 +1,29 @@
+import requests
+from globalVariables import base_url, generateEvaluationForm,companyUsername,password
+from loginTest import get_header
+url = base_url +generateEvaluationForm
+
+def evaluationForm(data,expected_status_code):
+    headers = get_header(companyUsername,password)
+    response = requests.post(url, json=data, headers=headers)
+    json_data = response.json()
+    assert response.status_code == expected_status_code, f"Expected status code {expected_status_code} found {response.status_code}"
+    expected_response_body = ["success", "status", "message", "data"]
+    for key in expected_response_body:
+        assert key in json_data, f"Expected key '{key}' in the response body"
+
+def unauthorizedAccess():
+    response = requests.post(url, json={"id":1})
+    json_data = response.json()
+    assert response.status_code == 401, f"Expected status code 401 found {response.status_code}"
+    expected_response_body = ["success", "status", "message"]
+    for key in expected_response_body:
+        assert key in json_data, f"Expected key '{key}' in the response body"
+#get evaluationForm
+evaluationForm({"trainingId":1},200)    
+#get evaluationForm for non existing training
+evaluationForm({"trainingId":10000},400) 
+#missing id
+evaluationForm({},500)
+#unauthorized access
+unauthorizedAccess()
